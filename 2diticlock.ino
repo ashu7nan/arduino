f0419e94d31490cacd34867e06d20ea0b0408ae0
ashu7nan <35619540+ashu7nan@users.noreply.github.com>
2020-08-28 16:32:46 +0530

    Add files via upload

---
+++ 2diticlock.ino
@@ -0,0 +1,86 @@
+#include<LiquidCrystal.h>
+//LiquidCrystal lcd(D0,D1,D2,D3,D4,D5);
+LiquidCrystal lcd(13,12,11,10,9,8,7);
+int mi=0;
+int sec=0;
+int hr=0;
+int flag=0;
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16,1);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i=0;i<60;i++)
+  {
+  
+  if(hr<10)
+  {
+    lcd.write('0');
+    lcd.print(hr,DEC);
+  }
+  else
+  {
+    lcd.print(hr,DEC);
+  }
+  lcd.write(':');
+   if(mi<10)
+  {
+    lcd.write('0');
+    lcd.print(mi,DEC);
+  }
+  else
+  {
+    lcd.print(sec,DEC);
+  }
+  lcd.write(':');
+   if(sec<10)
+  {
+    lcd.write('0');
+    lcd.print(sec,DEC);
+  }
+  else
+  {
+    lcd.print(sec,DEC);
+  }
+  
+  //lcd.print(sec,DEC);
+    
+   sec=sec+1;
+  
+  if(sec==60)
+  {
+    mi++;
+    sec=0;
+  }
+  if(mi==60)
+  {
+    hr++;
+    mi=0;
+  }
+   if(hr==12)
+  {
+    
+    hr=0;
+    if(flag==0)
+    {
+      flag=1;
+    }
+    else{
+      flag=0;
+    }
+  }
+  if(flag==0)
+  {
+    lcd.print(" AM");
+  }
+  else
+  {
+    lcd.print(" PM");
+  }
+  delay(1000);
+  lcd.clear();
+  }
+}
---
+++ slider_ok.ino
@@ -0,0 +1,86 @@
+#include<ESP8266WiFi.h>
+const char* ssid ="Abhijeet";
+const char* password="abhijeet";
+int value=HIGH;
+//To give the port number
+WiFiServer server(80);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(10);
+  //Connect to wifi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to");
+  Serial.println(ssid);
+  WiFi.begin(ssid,password);
+  while(WiFi.status() !=WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  //Start the server
+  server.begin();//Doubt
+  Serial.println("Server started");
+
+  //Print the IP Adress
+  Serial.print("Use this URL to connect:");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+  pinMode(D4,OUTPUT);
+  digitalWrite(D4,value);
+  }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Check if a client is connected 
+  WiFiClient client =server.available();
+  if(!client)
+  {
+    return;
+  }
+  //Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available())
+{
+  delay(1);
+}
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+  if(request.indexOf("led")!=-1)
+ {
+  int i=request.indexOf("led");
+  int j=request.indexOf("HTTP");
+  String ch=request.substring(i+3,j-1);
+  value=ch.toInt();
+  Serial.println(value,DEC);
+   analogWrite(D4,LOW);
+   // value=LOW;
+ }
+
+client.println("HTTP/1.1 200 OK");
+client.println("Connect-Type:text/html");
+client.println("");//do not forget
+client.println("<!DOCTYPE HTML>");
+client.println("<html><body>");
+//if(value==LOW)
+//{
+//  client.print("<font color='green'>ON<font>");
+//}
+//else
+//{
+//  client.print("<font color='red'>OFF<font>");
+//}
+client.print("<br><br>");
+client.print("<html><head><script>slide(){document.getElementById('link').href='led'+document.getElementById('slide').value}</script></head>");
+client.print("<body><a id='link'><input type='range' id='slide' onchange='slide()' min='0' max='1023'><\a></body></html>");
+delay(1);
+
+  
+
+}
